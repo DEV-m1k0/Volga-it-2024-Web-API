c0893793187e8542353153e4b405ce7900521cb0
Anton Knyazev <knyazevmac@icloud.com>
2024-09-24 13:53:13 +0300

    ready api for account

--- core/account/models.py
+++ core/account/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+
+# Список ролей
+CHOICES_ROLE_FOR_MYUSER = [
+    ('admin', 'Admin'),
+    ('manager', 'Manager'),
+    ('doctor', 'Doctor'),
+    ('user', 'User')
+]
+
+
+class Role(models.Model):
+    role = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return str(self.role)
+
+
+class MyUser(AbstractUser):
+    """
+    Модель с пользователями
+    """
+
+    lastName = models.CharField(max_length=30)
+    firstName = models.CharField(max_length=30)
+    roles = models.ManyToManyField(Role, blank=True, serialize=True)
+
+    def __str__(self) -> str:
+        return str(self.username)
--- core/core/settings.py
+++ core/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,6 +31,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,16 +40,64 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    # Дополнительные компоненты
+    # NOTE Дополнительные компоненты
     'rest_framework',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
 
-    # Мои микросервисы
+    # NOTE Мои микросервисы
     'api',
     'account',
     'hospital',
     'time_table',
-    'document'
+    'document',
+    'authentication'
 ]
+
+
+# NOTE модель для авторизации пользователей
+AUTH_USER_MODEL = 'account.MyUser'
+
+
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+
+    ],
+
+}
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+
+    'AUTH_HEADER_TYPES': ('Token',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': datetime.timedelta(days=1),
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
