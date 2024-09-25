f9ee2484335159083afa63f33ded59d61cca0026
Anton Knyazev <knyazevmac@icloud.com>
2024-09-25 21:12:08 +0300

    POST /api/Timetable

--- core/time_table/views.py
+++ core/time_table/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.views import APIView
+from .models import TimeTable
+from .logic.time_table import create_time_table
+
 
 # Create your views here.
+
+
+class TimeTableAPIView(APIView):
+    def post(self, request: Request):
+
+        response = create_time_table(request=request)
+
+        return response
