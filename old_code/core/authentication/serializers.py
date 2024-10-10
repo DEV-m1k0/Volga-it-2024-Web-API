c3b08724fcef38671510300ecccd85c622848f6e
Anton Knyazev <knyazevmac@icloud.com>
2024-10-10 19:51:44 +0300

    Добавлен и настроен новый микросервис 'Document' Была реализована базовая настройка и теперь можно приступать к разработке API

--- old_code/core/authentication/serializers.py
+++
@@ -1,8 +0,0 @@
-from account.models import MyUser
-from rest_framework import serializers
-
-class SignUpSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = MyUser
-        fields = ['lastName', 'firstName', 'username', 'password']
--- old_code/core/time_table/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import TimeTableAPIView, TimeTableByDoctorAPIVIew
-
-
-urlpatterns = [
-    path('', TimeTableAPIView.as_view()),
-    path("<int:id>/", TimeTableAPIView.as_view()),
-    path("Doctor/<int:id>/", TimeTableByDoctorAPIVIew.as_view())
-]
