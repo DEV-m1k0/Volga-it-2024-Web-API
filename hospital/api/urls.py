244c0ecefab0cb09ae842cb2ef433cb1c9e6e913
Anton Knyazev <knyazevmac@icloud.com>
2024-10-15 15:41:40 +0300

    Микросервис 'Account' был обновлен

--- hospital/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import HospitalsAPIView, RoomsByIdAPIView
-
-urlpatterns = [
-    path('Hospitals/', HospitalsAPIView.as_view()),
-    path('Hospitals/<int:id>/', HospitalsAPIView.as_view()),
-    path('Hospitals/<int:id>/Rooms/', RoomsByIdAPIView.as_view())
-]
